efde06dfaa237d4c25112c7f9d05878f23fb074d
Vinodambarapu267 <ambarapuvinod@gmail.com>
2024-08-10 12:16:14 +0530

    DataTypes

---
+++ src/com/javaproject/java/DataTypes.java
@@ -0,0 +1,29 @@
+package com.javaproject.java;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte Age;
+        Age = 12;
+        System.out.println("Age : " + Age);
+        short Salary;
+        Salary = 3500;
+        System.out.println("Salary : " + Salary);
+        long PhoneNo;
+        PhoneNo = 9573508320L;
+        System.out.println("Phone Number : " + PhoneNo);
+        int AnnualIncome = 1000000;
+        System.out.println( "Annual Income : " + AnnualIncome);
+        float Discount;
+        Discount = 23.4f;
+        System.out.println("Discount : "  + Discount);
+        double Savings;
+        Savings = 24.4444;
+        System.out.println("Saving : " + Savings);
+        boolean IsEligible;
+        IsEligible = true;
+        System.out.println("IsEligible : " + IsEligible);
+        char Gender;
+        Gender = 'M';
+        System.out.println("Gender : " + Gender);
+    }
+}
---
+++ src/com/javaproject/java/TypeCasting.java
@@ -0,0 +1,16 @@
+package com.javaproject.java;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        int age;
+        age = 32;
+         int ageValue;
+         ageValue = (int)age;
+         float a = 230304.4f;
+         int b = (int)a;
+        System.out.println("float Into Int : " + b);
+        ageValue = 130;
+        age = (byte)ageValue;
+        System.out.println(" int to byte : " + age);
+    }
+}
