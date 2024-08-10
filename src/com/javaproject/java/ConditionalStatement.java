004879971556e31ab1f3e50bf36d61e3508ecc50
Vinodambarapu267 <ambarapuvinod@gmail.com>
2024-08-10 12:32:07 +0530

    ConditionalStatements

---
+++ src/com/javaproject/java/ConditionalStatement.java
@@ -0,0 +1,26 @@
+package com.javaproject.java;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int productPrice;
+        productPrice = 30000;
+        int discount;
+        char memberShip;
+        int discountPercentage;
+        memberShip ='G';
+        if (memberShip == 'D'){
+            discountPercentage = 20;
+        } else if (memberShip == 'G') {
+            discountPercentage = 15;
+        } else if (memberShip == 'S') {
+            discountPercentage = 10;
+        }else {
+            discountPercentage = 5;
+        }
+        discount = productPrice * discountPercentage / 100;
+        System.out.println(" Discount Price $ : " + discount);
+        int finalPrice;
+        finalPrice= productPrice -discount;
+        System.out.println("final Price : " + finalPrice);
+    }
+}
