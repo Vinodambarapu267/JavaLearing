8c44550181a69f33fd7b1e1f01c50cca1630796d
Vinodambarapu267 <ambarapuvinod@gmail.com>
2024-08-12 23:25:30 +0530

    Methodtypes

---
+++ src/OOP/Customer.java
@@ -0,0 +1,46 @@
+package OOP;
+
+public class Customer {
+    int id;
+    String name;
+    String rollNo;
+    String PhoneNO;
+    String email;
+    int salary;
+    byte grade;
+
+    /*
+    without return type and without parameter
+     */
+    void displayDetails(){
+        System.out.println("id  : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("Salery : "  + salary);
+        System.out.println("Phone Number : " + PhoneNO);
+        System.out.println("Grade : " + grade);
+        System.out.println("Email : " + email);
+        System.out.println("Roll Number : " + rollNo);
+    }
+    /* without ReturnType and with Parameter
+     */
+    void displayMembership(String memberShip){
+        System.out.println("Display membershipType : " + memberShip);
+    }
+/*
+with returnType  and No paramters
+ */
+    byte discountPercentageByGrade(){
+        System.out.println("discount Percentage  is called..");
+        byte discountPercentage = 0;
+        if (grade >=9)
+            discountPercentage = 25;
+         else if (grade >7 && grade <9)
+             discountPercentage = 20;
+         else if (grade >4 && grade <7 )
+             discountPercentage = 15;
+         else
+             discountPercentage = 5;
+         return discountPercentage;
+    }
+
+}
---
+++ src/OOP/Main.java
@@ -0,0 +1,43 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        x = 100;
+        Customer customer1 = new Customer();
+        customer1.id = 111;
+        customer1.name = "Vinod";
+        customer1.grade = 9;
+        customer1.PhoneNO = "+91-95735083200";
+        customer1.rollNo = "21HM1A0302";
+        customer1.email = "ambarapuvinod@gmail.com";
+        customer1.salary = 200000;
+        customer1.displayDetails();
+
+        Customer customer2 = new Customer();
+        customer2.id = 112;
+        customer2.name = "Konda Reddy";
+        customer2.grade = 8;
+        customer2.PhoneNO = "+91-989032484";
+        customer2.rollNo = "21HM1A0330";
+        customer2.email = "skfnaskfn@gmail.com";
+        customer2.salary = 3000000;
+        Customer customer3 =new Customer();
+        customer3.grade = 7;
+        customer3.name = "rama";
+        customer2.displayDetails();
+        /* without Returntype and with paramaterized
+         */
+        customer1.displayMembership("Diamond");
+        customer2.displayMembership("GOLD");
+        /* with Retuntype and without parameters
+         */
+         /*byte discountPercentage = customer1.discountPercentageByGrade();
+        System.out.println( customer1.name + " 's DiscountPercentage is : " + discountPercentage);
+        System.out.println( customer2.name + " 's DiscountPercentage is : " + discountPercentage);
+          */
+        System.out.println( customer1.name + " 's DiscountPercentage is : " + customer1.discountPercentageByGrade());
+        System.out.println( customer2.name + " 's DiscountPercentage is : " + customer2.discountPercentageByGrade());
+        System.out.println( customer3.name + " 's DiscountPercentage is : " + customer3.discountPercentageByGrade());
+    }
+}
