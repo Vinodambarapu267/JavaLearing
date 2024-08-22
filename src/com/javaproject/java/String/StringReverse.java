5c81ac9822a7194d122f7bbd481abed4ae7db954
Vinodambarapu267 <ambarapuvinod@gmail.com>
2024-08-22 10:09:05 +0530

    WorkingwithStrings

--- src/com/javaproject/java/String/StringReverse.java
+++ src/com/javaproject/java/String/StringReverse.java
@@ -1,4 +1,12 @@
 package com.javaproject.java.String;
 
 public class StringReverse {
+    public static void main(String[] args) {
+        String username ="Vinod";
+        String reverse = "";
+        for (int i = 0; i<username.length() ; i++){
+         reverse = username.charAt(i) + reverse;
+        }
+        System.out.println("reversed Username: "+reverse);
+    }
 }
